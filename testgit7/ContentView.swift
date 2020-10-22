f4fd3032f29f608ea0c40f9e1b0be0b1cbe7643c
hadohado <hadohado@gmail.com>
2020-10-22 12:59:32 -0700

    create pallets of emoji inside ContentView, but should actually create palette inside ViewModel

--- testgit7/ContentView.swift
+++ testgit7/ContentView.swift
@@ -4,6 +4,8 @@
 //
 //  Created by ha tuong do on 10/22/20.
 //
+//  \.self is key-path that identify a variabl inside a object
+// in this case \.self identify itself in the String object
 
 import SwiftUI
 
@@ -12,7 +14,12 @@ struct ContentView: View {
     var body: some View {
         VStack {
             HStack {
-                Text(pallete)
+                // Text(pallete)
+                ForEach(pallete.map { String($0) }, id: \.self ) {
+                    emoj in Text(emoj)
+                }
+                // ForEach  = this computes views on demand from an underlying collection of of identified data.
+                // struct ForEach<Data, ID, Content> where Data : RandomAccessCollection, ID : Hashable
             }
             Rectangle().foregroundColor(.yellow)
         }
