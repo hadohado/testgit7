42a9b014a9aad88238b975a7dd2bed07d8a5c9ca
hadohado <hadohado@gmail.com>
2020-10-22 11:12:09 -0700

    Initial Commit

---
+++ testgit7/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testgit7
+//
+//  Created by ha tuong do on 10/22/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hi, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testgit7/testgit7App.swift
@@ -0,0 +1,17 @@
+//
+//  testgit7App.swift
+//  testgit7
+//
+//  Created by ha tuong do on 10/22/20.
+//
+
+import SwiftUI
+
+@main
+struct testgit7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
